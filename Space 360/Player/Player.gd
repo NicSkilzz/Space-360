184075edf02fb2a04578e773d3fcdedaf9bceba0
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2023-02-17 12:34:25 +0100

    Basic of shooting
    Just adds the basic of shooting even if debugging is necessary to make it better

--- Space 360/Player/Player.gd
+++ Space 360/Player/Player.gd
@@ -3,13 +3,33 @@ extends KinematicBody2D
 export (int) var SPEED = 200
 export (int) var ACCELERATION = 10
 export (int) var FRICTION = 1
+export (int) var bullet_speed = 1000
+export var fire_rate = 20000
 
 var input_direction = Vector2()
 var velocity = Vector2()
 
+var bullet = preload("res://Player/Bullet.tscn")
+var can_fire = true
+
 #onready var engine_sound = $EngineSound
 func _process(delta):
 	look_at(get_global_mouse_position())
+	#it seems like the deformity is caused because we set scale to 1, meaning that it can not use antiscaling to
+	#make the borders look smoth, in turn deforming the sprite. So while we can use 16x16 pixel art we have to upscale
+	#everything in order to make roation look smoth
+	
+	if Input.is_action_pressed("fire"):
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $BulletPoint.get_global_position()
+		# Change the position to make the shooting start at the edge 
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed, 0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
+	
 
 
 func get_input_direction():
