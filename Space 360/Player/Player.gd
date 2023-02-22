ee3ba098867c608bea7a8f31aca06d4e17e3d1c9
NicSkilzz <nklin02@gmail.com>
2023-02-22 14:08:04 +0100

    Movement Sound leiser

--- Space 360/Player/Player.gd
+++ Space 360/Player/Player.gd
@@ -73,7 +73,7 @@ func acceleration(direction):
 func movement_sound_volume_play(velocity):
 	var velocity_length_pre = pow(velocity.x, 2.0) + pow(velocity.y, 2.0) 
 	var velocity_length = pow(velocity_length_pre, 1/2.0)
-	var movement_volume = velocity_length / SPEED * 30 - 30
+	var movement_volume = velocity_length / SPEED * 30 - 40
 	AudioServer.set_bus_mute(movement_bus, false)
 	AudioServer.set_bus_volume_db(movement_bus, movement_volume)
 
