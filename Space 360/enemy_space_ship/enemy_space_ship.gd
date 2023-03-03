2cdc8a90f0334492712b09de5821269e948fb440
NicSkilzz <nklin02@gmail.com>
2023-03-03 10:10:27 +0100

    Enemy moves to player position

--- Space 360/enemy_space_ship/enemy_space_ship.gd
+++ Space 360/enemy_space_ship/enemy_space_ship.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 export (int) var ENEMY_HEALTH = 100
+export (int) var ENEMY_SPEED = 50
+var motion = Vector2.ZERO
+var player = null
 
 onready var enemy_health_bar = $enemy_health_bar
 
@@ -11,3 +14,18 @@ func _on_enemy_space_ship_area_area_entered(area):
 		enemy_health_bar.value = ENEMY_HEALTH
 	if ENEMY_HEALTH == 0:
 		queue_free()
+
+
+func _physics_process(delta):
+	motion = Vector2.ZERO
+	if player:
+		motion = position.direction_to(player.position) * ENEMY_SPEED
+	motion = move_and_slide(motion)
+
+func _on_enemy_scope_body_entered(body):
+	if body.name == "Player":
+		player = body
+
+func _on_enemy_scope_body_exited(body):
+	if body.name == "Player":
+		player = null
