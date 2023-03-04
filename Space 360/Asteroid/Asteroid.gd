2e3a2c7a2091690de4383cf191d486a5cd029529
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2023-03-04 18:36:09 +0100

    imortal asteroids

--- Space 360/Asteroid/Asteroid.gd
+++ Space 360/Asteroid/Asteroid.gd
@@ -15,11 +15,11 @@ func _ready():
 
 
 func _on_asteroid_area_area_entered(area):
-	if area.name == "bullet_area":
-		HEALTH -= 1
-		area.get_parent().queue_free()
-	if HEALTH == 0:
-		queue_free()
+	#if area.name == "bullet_area":
+		#HEALTH -= 1
+		#area.get_parent().queue_free()
+	#if HEALTH == 0:
+		#queue_free()
 		
 	if area.name == "DeathArea":
 		queue_free()
