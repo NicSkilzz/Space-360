45d624f0943ced4378355aa38c4185fcd28310bd
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2023-03-04 16:33:43 +0100

    Little tweaks

--- Space 360/World.gd
+++ Space 360/World.gd
@@ -21,21 +21,27 @@ func _ready():
 
 
 func _on_SpawnTimer_timeout():
-	#var enemy = Enemy.instance()
-	#add_child(enemy)
-	#enemy.position = $ShipSpawn.position
-	
-	
 	
 	#change Spawn position
 	var nodes = get_tree().get_nodes_in_group("spawn")# selec the spawn group
-	var node = nodes[randi() % nodes.size()]# chose a random node in the spawn group
+	var rng3 = RandomNumberGenerator.new()
+	rng3.randomize()
+	var node = nodes[rng3.randf_range(0.0, nodes.size())]# chose a random node in the spawn group
 	
 	# Set Spawn position to new position
 	var position = node.position
 	$ShipSpawn.position = position
 	
-	#$ShipSpawnTimer.wait_time = randi() % 5 + 5 # select random time between 5s and 10s
+	
+	var enemy = Enemy.instance()
+	add_child(enemy)
+	enemy.position = $ShipSpawn.position
+	
+	
+	
+	var rngT = RandomNumberGenerator.new()
+	rngT.randomize()
+	$ShipSpawnTimer.wait_time = rngT.randf_range(2,6) # select random time between 5s and 10s
 	
 
 
@@ -61,6 +67,7 @@ func _on_AsteroidSpawnRimer_timeout():
 	
 	
 	var rngT = RandomNumberGenerator.new()
+	rngT.randomize()
 	$AsteroidSpawnRimer.wait_time = rngT.randf_range(2,6) # select random time between 5s and 10s
 	
 	
