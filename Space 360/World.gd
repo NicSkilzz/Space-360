f6eee28adfae2c7ece10d2667a3b56ac79d79fba
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2023-03-08 16:18:51 +0100

    better astrewoid spawner

--- Space 360/World.gd
+++ Space 360/World.gd
@@ -7,6 +7,7 @@ var Enemy = preload("res://enemy_space_ship/enemy_space_ship_rigid_body.tscn")
 onready var asteroid_timer = $AsteroidSpawnRimer
 onready var ship_timer = $ShipSpawnTimer
 onready var current_score = $score_board/current_score
+onready var main_player = $Player
 
 export (int) var score = 0
 export (int) var end_score = 0
@@ -27,18 +28,18 @@ func _process(delta):
 
 
 func _on_SpawnTimer_timeout():
-	
+
 	#change Spawn position
 	var nodes = get_tree().get_nodes_in_group("spawn")# select the spawn group
 	var rng3 = RandomNumberGenerator.new()
 	rng3.randomize()
 	var node = nodes[rng3.randf_range(0.0, nodes.size())]# chose a random node in the spawn group
-	
+
 	var enemy = Enemy.instance()
 	add_child(enemy)
 	enemy.position = node.position
-	
-	ship_timer.wait_time = 1/((0.001 * time_elapsed) + 0.1) # select time
+
+	ship_timer.wait_time = 1/((0.006 * time_elapsed) + 0.1) # select time
 
 func _on_AsteroidSpawnRimer_timeout():
 		#change Spawn position
@@ -56,9 +57,23 @@ func _on_AsteroidSpawnRimer_timeout():
 	rng.randomize()
 	var rng2 = RandomNumberGenerator.new()
 	rng2.randomize()
-	asteroid.add_force(Vector2(), Vector2(rng.randf_range(-asteroid_speed_range, asteroid_speed_range),rng2.randf_range(-asteroid_speed_range, asteroid_speed_range)))
+	var node_x = node.global_position.x
+	var node_y = node.global_position.y
+	#asteroid.add_force(Vector2(), Vector2(rng.randf_range(-asteroid_speed_range, asteroid_speed_range),rng2.randf_range(-asteroid_speed_range,asteroid_speed_range)))
+	var player_position_x = main_player.global_position.x
+	var player_position_y = main_player.global_position.y
+	#asteroid.add_force(Vector2(),Vector2(rng2.randf_range(-asteroid_speed_range,asteroid_speed_range),0).rotated(atan2(player_position_x - node.global_position.x, player_position_y - node.global_position.y)))
 	
-
+	if(node_y <= player_position_y && node_x >= player_position_x):
+		asteroid.add_force(Vector2(), Vector2(rng.randf_range(-asteroid_speed_range, 0),rng2.randf_range(0,asteroid_speed_range)))
+	elif(node_y <= player_position_y && node_x <= player_position_x):
+		asteroid.add_force(Vector2(), Vector2(rng.randf_range(0, asteroid_speed_range),rng2.randf_range(0,asteroid_speed_range)))
+	elif(node_y >= player_position_y && node_x >= player_position_x):
+		asteroid.add_force(Vector2(), Vector2(rng.randf_range(-asteroid_speed_range, 0),rng2.randf_range(-asteroid_speed_range,0)))
+	else:
+		asteroid.add_force(Vector2(), Vector2(rng.randf_range(0, asteroid_speed_range),rng2.randf_range(-asteroid_speed_range,0)))
+		
+		
 	asteroid_timer.wait_time = 1/((0.001 * time_elapsed) + 0.1) # select time
 
 func load_highscore():
